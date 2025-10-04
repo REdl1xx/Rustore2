a40fed786df64ea5e6437e1021411ecc085382cc
user <playsmain@yandex.ru>
2025-10-04 20:09:58 +0700

    Нафигация

--- app/src/main/java/com/example/rustore2/uii/functions.kt
+++ app/src/main/java/com/example/rustore2/uii/functions.kt
@@ -4,16 +4,21 @@ import android.annotation.SuppressLint
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import kotlinx.serialization.Serializable
 
 @Serializable
-object Profile
+object SearchBar
 @Serializable
-object FriendsList
+object AppCard
 
 
 @Composable
 fun Rootscreen() {
     val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = SearchBar){
+        composable<SearchBar>{}
+    }
 }
