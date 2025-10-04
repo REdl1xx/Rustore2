f5df65c9555b3a383b3c832b4e6f0481de38d595
REdl1xx <147504673+REdl1xx@users.noreply.github.com>
2025-10-04 17:22:26 +0700

    Поисковая строка кнопка c эффектом

--- app/src/main/java/com/example/rustore2/uii/elementsmainapp/Searchbar.kt
+++ app/src/main/java/com/example/rustore2/uii/elementsmainapp/Searchbar.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -28,13 +29,12 @@ fun SearchBar(onClick: () -> Unit){
             .padding(horizontal = 16.dp, vertical = 8.dp)
             .fillMaxWidth()
             .height(50.dp)
+            .clip(RoundedCornerShape(25.dp))
+            .clickable { onClick() }
             .background(
                 color = Color.LightGray,
                 shape = RoundedCornerShape(25.dp)
-            )
-            .clickable { onClick() }
-
-        ,
+            ),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Spacer(modifier = Modifier.width(16.dp))
