99a1e0827a57fdace3e5c50236ece82f448f21bf
REdl1xx <147504673+REdl1xx@users.noreply.github.com>
2025-10-05 12:08:44 +0700

    онбординг

--- app/src/main/java/com/example/rustore2/uii/StartPage.kt
+++ app/src/main/java/com/example/rustore2/uii/StartPage.kt
@@ -3,8 +3,17 @@ package com.example.rustore2.uii
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import java.io.File
 import com.example.rustore2.MainActivity
@@ -14,7 +23,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -35,12 +47,24 @@ object Category
 @Composable
 fun Starter(onNavigateToMain: () -> Unit) {
     Column (modifier = Modifier
-        .background(color = Color.White)) {
-        Image(ImageBitmap.imageResource(R.drawable.rustore_icon), contentDescription = "rustore")
-        Text("Добро пожаловать в RuStore!")
-        Text("Welcome text example")
-        Button(onClick = {onNavigateToMain()}) {
+        .fillMaxSize()
+        .background(color = Color.White)
+    ) {
+        Spacer(modifier = Modifier.height(40.dp))
+        Image(ImageBitmap.imageResource(R.drawable.rustore_icon), contentDescription = "rustore", modifier = Modifier.fillMaxWidth())
+        Spacer(modifier = Modifier.height(16.dp))
+        Text("Добро пожаловать в RuStore!", modifier = Modifier.fillMaxWidth().padding(16.dp), fontSize = 24.sp, fontWeight = FontWeight.Bold)
+        Text("Получите доступ к множеству приложений и игр в РуСтор, загружайте безопасно и удобно. Ручная модерация и безопасные платежи. При поддержке Минцифры России.", modifier = Modifier.fillMaxWidth().padding(16.dp), fontSize = 16.sp, fontWeight = FontWeight.Normal)
+        Spacer(Modifier.weight(1f))
+        Button(onClick = {onNavigateToMain()},
+            modifier = Modifier
+                .background(color = Color.White)
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
             Text("Продолжить")
+            Spacer(Modifier.weight(1f))
+            Icon(Icons.Default.ArrowForward, contentDescription = "Продолжить")
         }
     }
 }
@@ -88,4 +112,10 @@ fun Main(check: Boolean) {
             )
         }
     }
+}
+
+@Preview
+@Composable
+fun StarterPreview() {
+    Starter(onNavigateToMain = {})
 }
